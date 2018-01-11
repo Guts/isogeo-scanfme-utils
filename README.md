911a0ee8efa83fbacb4d2859673141c466442e36
Guts <Guts@users.noreply.github.com>
2018-01-11 12:39:23 +0100

    add tests

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Isogeo Scan FME utils
 
+[![Build Status](https://travis-ci.org/Guts/isogeo-scanfme-utils.svg?branch=master)](https://travis-ci.org/Guts/isogeo-scanfme-utils) ![Python 3.5](https://img.shields.io/badge/python-3.5-blue.svg) ![Python 3.6](https://img.shields.io/badge/python-3.6-blue.svg)
+
 A quicky class to perform some regular operations on Isogeo Scan FME database.
+
---
+++ tests/test_authentication.py
@@ -0,0 +1,60 @@
+# -*- coding: UTF-8 -*-
+#! python3
+from __future__ import (absolute_import, print_function, unicode_literals)
+
+# #############################################################################
+# ########## Libraries #############
+# ##################################
+
+# Standard library
+from os import environ
+import unittest
+
+import pymongo
+from .scanfme_db_utils import IsogeoScanUtils
+
+# #############################################################################
+# ######## Globals #################
+# ##################################
+
+access = {
+          "username": environ.get("username"),
+          "password": environ.get("password"),
+          "server": environ.get("server"),
+          "port": environ.get("port"),
+          "db_name": environ.get("db_name"),
+          "replicaSet": environ.get("replicaSet"),
+          }
+
+# #############################################################################
+# ######## Classes #################
+# ##################################
+
+
+class DbAuthentication(unittest.TestCase):
+    """Test authentication process."""
+
+    # standard methods
+    def setUp(self):
+        """Executed before each test."""
+        pass
+
+    def tearDown(self):
+        """Executed after each test."""
+        pass
+
+    # tests
+    def test_db_auth(self):
+        """API secret must be 64 length."""
+        app = IsogeoScanUtils(access=access,
+                              def_wg=environ.get("wg_test"),
+                              platform="qa")
+        cli = app.connect()
+        self.assertIsInstance(cli, pymongo.mongo_client.MongoClient)
+
+# #############################################################################
+# ######## Standalone ##############
+# ##################################
+
+if __name__ == '__main__':
+    unittest.main()
